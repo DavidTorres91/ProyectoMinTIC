1adef90325d46e70deec119accad4f8ac3d6fee5
JhonnyPG <111162781+JhonnyPG@users.noreply.github.com>
2022-08-31 17:25:41 -0500

    consola

---
+++ AplicacionWebG3.Consola/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AplicacionWebG3.Dominio;
+using AplicacionWebG3.Persistencia;
+
+namespace AplicacionWebG3.Consola
+{
+    public class Program
+    {
+        private static ITipoDocumentoRepository _tipoDocumentoRepository = new TipoDocumentoRepository(new AplicacionWebG3.Persistencia.AppC());
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bienvenido:");
+            var tipoDocumento = new TipoDocumento{
+                Nombre = "Cedula"
+            };  
+            var result = _tipoDocumentoRepository.Add(tipoDocumento);
+
+            if (result > 0) {
+                Console.WriteLine("Se inserto Correctamente");
+            }else
+            {
+               Console.WriteLine("no se inserto Correctamente"); 
+            }
+
+        }
+    }
+}
+
